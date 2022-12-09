0068badfae053a0406acf5b3d2be7fb3b14c43df
SankethPhoenix <sankethphoenix21@gmail.com>
2022-12-09 19:26:18 +0530

    Ex1 problem

--- JavaEx1.java
+++ JavaEx1.java
@@ -1,4 +1,49 @@
+class Number {
+    private int num;
+
+    Number(int num) {
+        this.num = num;
+    }
+
+    boolean isZero() {
+        return this.num == 0;
+    }
+
+    boolean isPositive() {
+        return this.num > 0;
+    }
+
+    boolean isNegative() {
+        return this.num < 0;
+    }
+
+    boolean isOdd() {
+        return (this.num % 2) == 1;
+    }
+
+    boolean isEven() {
+        return (this.num % 2) == 0;
+
+    }
+
+    boolean isPrime() {
+        for (int i = 1; i <= this.num / 2; i++) {
+            if (this.num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    boolean isAmstrong() {
+        return (true);
+    }
+}
 
 public class JavaEx1 {
+    public static void main(String[] args) {
+        Number n1 = new Number(5);
+        System.out.println(n1.isZero());
+    }
 
 }
