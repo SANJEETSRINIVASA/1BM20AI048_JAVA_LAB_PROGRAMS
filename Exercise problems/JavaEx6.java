0505a99970a557af240c572e5a11ef459fec12bd
SankethPhoenix <sankethphoenix21@gmail.com>
2022-12-10 10:54:15 +0530

    java program

--- Exercise problems/JavaEx6.java
+++
@@ -1,3 +0,0 @@
-public class JavaEx6 {
-
-}
---
+++ ExerciseProblems/JavaEx6.java
@@ -0,0 +1,9 @@
+import Employee.*;
+
+public class JavaEx6 {
+    public static void main(String[] args) {
+        Emp e = new Emp("ANU", "23", "Female", 12000);
+        e.calculate();
+        e.display();
+    }
+}
